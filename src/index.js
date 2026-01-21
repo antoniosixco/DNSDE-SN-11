6eb6900115e0ff10e241a65c578116e1ec06da6e
Alberto Villegas <168059876+Gigoalb@users.noreply.github.com>
2026-01-21 00:06:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("App Node.js en producción 🚀");
+  res.status(200).send("App Node.js en producción 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+module.exports = app;
