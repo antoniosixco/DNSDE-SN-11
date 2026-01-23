6c76905e8f37c42ca2597eceb1fb8856826d2256
Alberto Villegas <168059876+Gigoalb@users.noreply.github.com>
2026-01-23 17:43:50 -0600

    Update index.test.js
    2 tests que pasan y uno que no

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,16 +1,24 @@
 const request = require("supertest");
-const app = require("../src/index");
+const app = require("../src/server");
 
 describe("API Endpoints", () => {
+  // ✅ Prueba que PASA
   test("GET / debe responder correctamente", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("API Node.js funcionando 🚀");
   });
 
+  // ✅ Prueba que PASA
   test("GET /health debe responder estado ok", async () => {
     const res = await request(app).get("/health");
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe("ok");
+  });
+
+  // ❌ Validación de ERROR
+  test("GET /ruta-inexistente debe retornar 404", async () => {
+    const res = await request(app).get("/ruta-inexistente");
+    expect(res.statusCode).toBe(404);
   });
 });
