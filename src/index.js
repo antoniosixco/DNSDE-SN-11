ff0148c341277b5fe6baf1f8d115584199e2ac59
Sam <105896750+slopez6@users.noreply.github.com>
2026-03-01 23:21:32 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Esta app ha sido destruida(Test-fail) 💀");
+  res.status(200).send("Prueba de webhook");
 });
 
 // --- ESTE ES EL HEALTH CHECK OBLIGATORIO ---
