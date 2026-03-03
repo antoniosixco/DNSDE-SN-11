2f72e0a00002276b4b6d41eab77f2cc94795774b
Sam <105896750+slopez6@users.noreply.github.com>
2026-03-02 21:26:09 -0600

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -11,7 +11,7 @@ describe("API Endpoints", () => {
   test("GET / debe responder correctamente", async () => {
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe("API Grupo SN-11 - Entorno de Produccion Activo");
+  expect(res.text).toBe("API Grupo SN-11 - Prueba para video");
 });
 
   // ✅ Prueba 2 (Ajustada al nuevo estándar)
